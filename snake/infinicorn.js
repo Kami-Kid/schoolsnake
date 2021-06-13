12ae37f27666b1d7892b22f86e8f514361c3c543
Kami-Kid <55991137+Kami-Kid@users.noreply.github.com>
2021-06-13 23:02:57 +0100

    Delete snake directory

--- snake/infinicorn.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', pluslength)
-
-function pluslength() {
-    var a = document.createElement("pre")
-    a.innerHTML = "           |||        |||"
-    a.className = "pre"
-    document.body.appendChild(a)
-    console.log("done")
-}
